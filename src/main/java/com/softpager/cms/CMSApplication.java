fc90a574782b2ef94ac0107c2e1f71c83f75292b
simsonites <samfeminite@gmail.com>
2019-02-04 07:24:09 +0700

    My first commit -- project skeleton

---
+++ src/main/java/com/softpager/cms/CMSApplication.java
@@ -0,0 +1,14 @@
+package com.softpager.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CMSApplication.class, args);
+    }
+
+}
+
